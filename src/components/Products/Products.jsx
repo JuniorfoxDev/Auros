2d70318aeaae6778c83bfa0d5a2f76d6d18ad1a0
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-17 21:32:54 +0000

    git push

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -31,7 +31,7 @@ const Products = () => {
         <h2 className='text-3xl font-bold  py-6'>All Products</h2>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10 md:px-0'>
         {products.slice(0,7).map((items,i)=> {
-            return <ProductCard key={i} id={items.id} name={items.name} img={items.images} price={items.price} old_price={items.old_price} />
+            return <ProductCard key={i} id={items.id} name={items.name} img={items.images[0].url} price={items.price} old_price={items.old_price} />
         })}
         </div>
         <div className='text-center'>
