be483e5872246fca3725a7392017f10df219cbeb
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-18 09:19:35 +0000

    git push

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 const ProductCard = ({images,name,price}) => {
-  
+  const imageUrl = images && images.length > 0 ? images[0].url : '/path-to-your-default-image.jpg';
   return (
     <NavLink>
       <div className='overflow-hidden transform duration-300 cursor-pointer hover:scale-95 px-4'>
-      <img src={images[0].url} alt="" className='rounded-lg w-[400px] h-[400px]'/>
+      <img src={imgUrl} alt="" className='rounded-lg w-[400px] h-[400px]'/>
       <div className='p-2 md:p-4 text-black/[0.9]'>
         <h2 className='text-sm md:text-base font-semibold'>{name}</h2>
         <div className='flex items-center text-black/[0.5]'>
