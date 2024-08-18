4e18d18decd38aeb01c26e1319b1ff95c9d9d6e3
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-18 09:24:37 +0000

    git push

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -30,8 +30,17 @@ const Products = () => {
     <div className='h-auto px-3 md:px-28 py-14'>
         <h2 className='text-3xl font-bold  py-6'>All Products</h2>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10 md:px-0'>
-        {products.slice(0,7).map((items,i)=> {
-            return <ProductCard key={products.id} {...products} />
+        {products.slice(0,7).map(()=> {
+            <div className='overflow-hidden transform duration-300 cursor-pointer hover:scale-95 px-4'>
+            <img src={products.images[0].url} alt="" className='rounded-lg w-[400px] h-[400px]'/>
+            <div className='p-2 md:p-4 text-black/[0.9]'>
+              <h2 className='text-sm md:text-base font-semibold'>{products.name}</h2>
+              <div className='flex items-center text-black/[0.5]'>
+              <p className='mr-2 text-lg font-bold'>&#8377; {products.price}</p>
+              {/* <p className='text-base font-medium line-through'>&#8377; {old_price}</p> */}
+              </div>
+            </div>
+            </div>
         })}
         </div>
         <div className='text-center'>
