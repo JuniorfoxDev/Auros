8442829dd03cbc15bfb0cb8ee386e4d09b0eb88b
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-18 09:15:54 +0000

    git push

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,16 +3,17 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
-const ProductCard = (props) => {
+const ProductCard = ({images,name,price}) => {
+  
   return (
     <NavLink>
       <div className='overflow-hidden transform duration-300 cursor-pointer hover:scale-95 px-4'>
-      <img src={props.images[0].url} alt="" className='rounded-lg w-[400px] h-[400px]'/>
+      <img src={images[0].url} alt="" className='rounded-lg w-[400px] h-[400px]'/>
       <div className='p-2 md:p-4 text-black/[0.9]'>
-        <h2 className='text-sm md:text-base font-semibold'>{props.name}</h2>
+        <h2 className='text-sm md:text-base font-semibold'>{name}</h2>
         <div className='flex items-center text-black/[0.5]'>
-        <p className='mr-2 text-lg font-bold'>&#8377; {props.price}</p>
-        <p className='text-base font-medium line-through'>&#8377; {props.old_price}</p>
+        <p className='mr-2 text-lg font-bold'>&#8377; {price}</p>
+        {/* <p className='text-base font-medium line-through'>&#8377; {old_price}</p> */}
         </div>
       </div>
       </div>
